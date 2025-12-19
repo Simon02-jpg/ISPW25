6863c37208db55bcf3aa79d0bbed428bf308eb83
Simon02-jpg <118351181+Simon02-jpg@users.noreply.github.com>
2025-12-19 18:58:22 +0100

    Delete src directory

--- src/main/java/model/dao/GenericProcedureDAO.java
+++
@@ -1,9 +0,0 @@
-package model.dao;
-
-import java.sql.SQLException;
-
-import model.dao.exception.DAOException;
-
-public interface GenericProcedureDAO<P> {
-    P execute(Object... params) throws SQLException, DAOException;
-}
--- src/main/java/server/com/server/exception/PersonalException.java
+++
@@ -1,7 +0,0 @@
-package server.com.server.exception;
-
-public class PersonalException extends Exception {
-    public PersonalException(String errorMessage) {
-        super(errorMessage);
-    }
-}
